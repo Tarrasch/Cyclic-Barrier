23e80e8ae5893048238fe7200502e601176c32cf
Arash Rouhani <miffoljud@gmail.com>
2011-03-09 14:17:43 +0100

    Created Project2

---
+++ Cyclic-Barrier/src/cyclicbarrier/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cyclicbarrier;
+
+/**
+ *
+ * @author rarash
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+    
+}
